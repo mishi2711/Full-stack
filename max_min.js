09d03144a0d69f725b9842f2024f47441a24869c
mishi2711 <mishijohri@gmail.com>
2025-12-15 19:25:49 +0530

    this repo is now up to dated

---
+++ max_min.js
@@ -0,0 +1,6 @@
+const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+const maxNumber = Math.max(...numbers);
+const minNumber = Math.min(...numbers);
+
+console.log('Maximum number:', maxNumber);
+console.log('Minimum number:', minNumber);
